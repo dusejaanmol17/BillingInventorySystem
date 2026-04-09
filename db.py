c73782e661e6c2926c0c23aa4791cb3bb20a1274
Anmol Duseja <dusejaanmol17@gmail.com>
2026-04-09 15:34:14 +0530

    fixed DB Connection for render

--- db.py
+++ db.py
@@ -1,10 +1,8 @@
 import psycopg2
+import os
 
 def get_connection():
-    conn = psycopg2.connect(
-        host="localhost",
-        database="MusicWorldBillingDB",
-        user="postgres",
-        password="Anmol@2406"
-    )
+    DATABASE_URL = os.getenv("DATABASE_URL")
+
+    conn = psycopg2.connect(DATABASE_URL)
     return conn
